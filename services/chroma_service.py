4ad35508208bb6ce404fab7a31141774aa09598e
JALLMAR ANDRES SAMUELS TORRES <jallmar.samuels@ucr.ac.cr>
2025-05-22 13:33:47 -0600

    Cambios pequeños

--- services/chroma_service.py
+++ services/chroma_service.py
@@ -11,3 +11,12 @@ def store_embedding(document_id: str, embedding: list[float], metadata: dict):
         ids=[document_id],
         metadatas=[metadata]
     )
+    
+def get_all_embeddings():
+    collection = client.get_or_create_collection(name="document_embeddings")
+    data = collection.get()
+    return {
+        "ids": data["ids"],
+        "embeddings": data["embeddings"],
+        "metadatas": data["metadatas"]
+    }
